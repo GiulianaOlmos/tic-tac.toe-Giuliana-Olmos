a40fe84e5c7bd813362fbb55a920b296b7b43ecc
Giuliana <giulianaolmos@gmail.com>
2020-01-06 17:24:35 -0300

    creando tablero  y movimientos

--- static/juego.js
+++ static/juego.js
@@ -1,5 +1,6 @@
 var tituloDinamico = document.querySelector('#titulo-idica-accion');
 var formularioEligeJugador = document.querySelector('#elige-jugador');
+var tablero = document.querySelector('#tablero');
 
 //Junto los dos inputs en un array para recorrerlos, y de esta forma condicionar el evento click.
 var jugadores = Array.prototype.slice.call(document.querySelectorAll('input[name=Elecciones]'));
@@ -15,6 +16,47 @@ function setearJugadores(){
 }
 
 function crearTablero(){
-    
+  for(var i=0; i<9; i++){
+    var casilla = document.createElement('div');
+    casilla.id = "casilla"+i;
+    casilla.className = "casillas"
+    casilla.addEventListener('click', movimientoJugador, false);
+    tablero.appendChild(casilla);
+  }
+  casillas = Array.prototype.slice.call(document.querySelectorAll('.casillas'));
+}
+
+function movimientoJugador(){
+  if(this.textContent === ""){
+    this.textContent = jugador;
+  }
+  if(esGanador()){
+    swal(" El ganador es "+ jugador);
+  };
+}
+
+function esGanador(){
+  ganador = false;
+  var combinaciones = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+  ];
+  for(var i = 0; i < combinaciones.length; i++){
+    combinacion = combinaciones[i]
+    if(
+      document.getElementById('casilla'+combinacion[0]).textContent === jugador &&
+      document.getElementById('casilla'+combinacion[1]).textContent === jugador &&
+      document.getElementById('casilla'+combinacion[2]).textContent === jugador
+    ){
+      ganador = true;
+    }
+  }
+  return ganador
 }
     
