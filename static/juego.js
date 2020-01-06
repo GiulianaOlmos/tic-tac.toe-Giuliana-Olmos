e35e0521e3ba5b7dac9b0b5c481a07ad953002a8
Giuliana <giulianaolmos@gmail.com>
2020-01-06 13:19:05 -0300

    setear jugadores

--- static/juego.js
+++ static/juego.js
@@ -1,17 +1,20 @@
-var tituloDinamico = document.querySelector('.titulo-idica-accion');
-var jugadorX = document.querySelector('.jugador-1');
-var jugadorO = document.querySelector('.jugador-2');
+var tituloDinamico = document.querySelector('#titulo-idica-accion');
+var formularioEligeJugador = document.querySelector('#elige-jugador');
 
+//Junto los dos inputs en un array para recorrerlos, y de esta forma condicionar el evento click.
+var jugadores = Array.prototype.slice.call(document.querySelectorAll('input[name=Elecciones]'));
+jugadores.forEach(function(choice){
+  choice.addEventListener('click', setearJugadores, false);
+});
 
-function startGame(){
-    if(elegirJugador()){
-        tituloDinamico.innerHTML ="Juegas con " +  elegirJugador();
-    }
+function setearJugadores(){
+    jugador = this.value; //le seteamos el valor del input al cual se le hizo click
+    tituloDinamico.textContent = "Juegas con " + jugador;
+    formularioEligeJugador.style.display = "none";
+    crearTablero();
 }
 
-function elegirJugador(){
-    jugadorX.addEventListener('click', function(){return "X"});
-    jugadorO.addEventListener('click', function(){return "O"})
+function crearTablero(){
+    
 }
-elegirJugador();
-console.log(elegirJugador())
+    
